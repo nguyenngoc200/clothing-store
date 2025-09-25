b43e225dae32357289d9df56f30eb45d2f09b1d3
Quinn28022003 <hahoangquan2@gmail.com>
2025-09-26 00:07:04 +0700

    [CT-001] improve: test

--- src/app/(private)/admin/page.tsx
+++ src/app/(private)/admin/page.tsx
@@ -1,3 +1,3 @@
 export default function AdminPage() {
-  return <div className="font-sans min-h-screen w-full">Admin Page</div>;
+  return <div className="font-sans min-h-screen w-full">Admin Page nè</div>;
 }
