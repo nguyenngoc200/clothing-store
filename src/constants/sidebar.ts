439a64e67864e422b2243f488820229de77bbd4d
Quinn28022003 <hahoangquan2@gmail.com>
2025-10-13 04:53:47 +0700

    [CT-003] feat: sidebar constants

---
+++ src/constants/sidebar.ts
@@ -0,0 +1,31 @@
+import { Calendar, Home, Inbox, Search, Settings } from 'lucide-react';
+
+export const SIDEBAR_ITEMS = [
+  {
+    title: 'Home',
+    url: '#',
+    icon: Home,
+  },
+  {
+    title: 'Inbox',
+    url: '#',
+    icon: Inbox,
+  },
+  {
+    title: 'Calendar',
+    url: '#',
+    icon: Calendar,
+  },
+  {
+    title: 'Search',
+    url: '#',
+    icon: Search,
+  },
+  {
+    title: 'Settings',
+    url: '#',
+    icon: Settings,
+  },
+];
+
+export type SidebarItem = (typeof SIDEBAR_ITEMS)[number];
