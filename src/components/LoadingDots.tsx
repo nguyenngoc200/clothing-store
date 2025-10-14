aba243ab1da41edd0eb696f0e2fe44a174602a72
Quinn28022003 <hahoangquan2@gmail.com>
2025-10-15 04:33:45 +0700

    [CT-004] feat: component LoadingDots

---
+++ src/components/LoadingDots.tsx
@@ -0,0 +1,55 @@
+import { cn } from '@/lib/utils';
+
+type TAnimationSpeed = 'slow' | 'normal' | 'fast';
+
+type TSpeedDurations = Record<TAnimationSpeed, string>;
+
+interface ILoadingDotsProps {
+  text?: string;
+  notText?: boolean;
+  textColor?: string;
+  dotColor?: string;
+  animationSpeed?: TAnimationSpeed;
+}
+
+export const LoadingDots = (props: ILoadingDotsProps) => {
+  const {
+    text = 'Đang tải',
+    textColor = '',
+    notText,
+    dotColor = 'text-current',
+    animationSpeed = 'fast',
+  } = props;
+
+  const speedDurations: TSpeedDurations = {
+    slow: '3s',
+    normal: '2s',
+    fast: '1s',
+  } as const;
+
+  const duration = speedDurations[animationSpeed];
+
+  return (
+    <span className="flex items-center" data-testid="loading-dots">
+      {!notText && (
+        <span className={cn('text-primary font-headline font-semibold tracking-wide', textColor)}>
+          {text}
+        </span>
+      )}
+      <span className="flex">
+        {[0, 1, 2].map((index) => (
+          <span
+            key={index}
+            className={dotColor}
+            style={{
+              animation: `dotAppear ${duration} infinite`,
+              animationDelay: `calc(${duration} / 3 * ${index})`,
+            }}
+          >
+            .
+          </span>
+        ))}
+      </span>
+    </span>
+  );
+};
