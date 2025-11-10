18f69c503d0d7c9bbb52ba9a17718f3706316bd8
Quinn28022003 <hahoangquan2@gmail.com>
2025-11-10 21:12:21 +0700

    [CT-007] feat: add ReportsPage component with ReportsDashboard integration

---
+++ src/app/(private)/admin/reports/page.tsx
@@ -0,0 +1,13 @@
+import ReportsDashboard from './ReportsDashboard';
+
+export const metadata = {
+  title: 'Báo cáo - Admin',
+};
+
+export default function ReportsPage() {
+  return (
+    <div>
+      <ReportsDashboard />
+    </div>
+  );
+}
