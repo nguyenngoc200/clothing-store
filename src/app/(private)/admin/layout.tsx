bc3db65cdd6a0983eaf3ba2b590b105fed2daff2
Quinn28022003 <hahoangquan2@gmail.com>
2025-11-10 21:10:40 +0700

    [CT-007] feat: refactor PrivateLayout to use AdminLayoutClient for improved layout structure

--- src/app/(private)/admin/layout.tsx
+++ src/app/(private)/admin/layout.tsx
@@ -1,9 +1,7 @@
 import { User } from '@supabase/supabase-js';
 import { notFound } from 'next/navigation';
 
-import HeaderAdmin from '@/components/admin/HeaderAdmin';
-import Sidebar from '@/components/sidebar/Sidebar';
-import { SidebarProvider } from '@/components/ui/sidebar';
+import AdminLayoutClient from './AdminLayoutClient';
 import { getUserServer } from '@/lib/supabase/getUserServer';
 import { EInternalRole } from '@/types/ERole';
 import { TProfile } from '@/types/TProfile';
@@ -24,15 +22,8 @@ export default async function PrivateLayout({ children }: { children: React.Reac
   }
 
   return (
-    <div className="flex flex-row">
-      <SidebarProvider>
-        <Sidebar user={user} />
-
-        <div className="flex-1 min-h-screen flex flex-col">
-          <HeaderAdmin user={user} />
-          <main className="flex-1 p-0 py-4 sm:py-0 sm:p-6">{children}</main>
-        </div>
-      </SidebarProvider>
-    </div>
+    <AdminLayoutClient user={user} profile={profile}>
+      <div className="container mt-5"> {children}</div>
+    </AdminLayoutClient>
   );
 }
